94f61154494d9ef0f31451da4768727b3dfb829a
jerico <jericosalvador@noreply.github.com>
2021-02-20 23:47:33 -0800

    edited info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -15,13 +15,18 @@ export function Info(){
                 a heuristic. In this demo the cost to go left, right, up, or down 
                 is 1. It takes one action to go left or right. 
             </p>
+            <p>
+                Learn more about A* on <a href="https://en.wikipedia.org/wiki/A*_search_algorithm">
+                    wikipedia.
+                </a>
+            </p>
             <h5>What is a heuristic?</h5>
             <p>
                 A heuristic gives an estimate of how far the goal is relative to each state. 
                 In this demo our heuristic is the euclidean distance to the goal.
             </p>
             <p>
-                Learn more about the heuristic on <a href="https://en.wikipedia.org/wiki/A*_search_algorithm">
+                Learn more about heuristics on <a href="https://en.wikipedia.org/wiki/Heuristic">
                     wikipedia.
                 </a>
             </p>
